a29884827b7167bc102a5e3ceeb93835914c2d49
Nadasheikh4 <147923829+Nadasheikh4@users.noreply.github.com>
2025-02-24 09:54:45 +0500

    Add files via upload

---
+++ py-challange-day1/main.py
@@ -0,0 +1,3 @@
+print("Hello, Python Learners! 🚀 Today is the first step in our coding journey")
+print("My name is Nada sheikh 🧕🏻 and I am a python learner")
+print("I am excited🤩 to learn👩🏻‍🎓 python with you all! 🎉")
